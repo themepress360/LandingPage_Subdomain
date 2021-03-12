826306eefa5293ec89a39183595ec0ccfeee32e6
themepress360 <themepress360@gmail.com>
2021-03-13 00:21:26 +0500

    code updated

--- app/Constants/constants.php
+++ app/Constants/constants.php
@@ -3,5 +3,5 @@ define('SendGridEmail','themepress360@gmail.com');
 define('APPLICATION_NAME','Superb Opportunity');
 define('API_VERSION','v1');
 define('API_LIMIT','100');
-define('AuthorizationToken','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');
+define('AuthorizationToken','');
 ?>
