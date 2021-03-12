60e47207e4d60999cb63f81e98831867a649db6a
PHP-Team <themepress360@gmail.com>
2021-03-13 00:06:53 +0500

    Email Template change

--- app/Constants/constants.php
+++ app/Constants/constants.php
@@ -1,6 +1,6 @@
 <?php
 define('SendGridEmail','themepress360@gmail.com');
-define('APPLICATION_NAME','Tastino');
+define('APPLICATION_NAME','Superb Opportunity');
 define('API_VERSION','v1');
 define('API_LIMIT','100');
 define('AuthorizationToken','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');
